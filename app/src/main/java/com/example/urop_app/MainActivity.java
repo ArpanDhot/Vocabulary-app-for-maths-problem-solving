686f93f3725206c3d36c6a674feffc0af674c31e
ArpanDhot <sarpanjot0@gmail.com>
2022-06-08 14:19:41 +0100

    1- Splash screen

--- app/src/main/java/com/example/urop_app/MainActivity.java
+++ app/src/main/java/com/example/urop_app/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.urop_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //This is hiding the hide the OS bar
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+        }
+
+        //To make the windows full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Setting up the activity
         setContentView(R.layout.activity_main);
+
+        //Creating a delay for splash screen
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                Intent intent = new Intent(getApplicationContext(),Menu.class);
+                startActivity(intent);
+            }
+        }, 5000);
+
     }
 }
