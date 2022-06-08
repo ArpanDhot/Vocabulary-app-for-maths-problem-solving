f2658d237e747d1d4bc06664ef2e6e8e32ed1292
ArpanDhot <sarpanjot0@gmail.com>
2022-06-08 11:08:51 +0100

    1- Created packages

---
+++ app/src/main/java/com/example/urop_app/Menu.java
@@ -0,0 +1,14 @@
+package com.example.urop_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
